1eaad57cd3e8f110a4616092bfdfeafe018a36bc
Sebastien Dupertuis <34378703+Sebdee@users.noreply.github.com>
2020-04-30 17:35:52 +0200

    Version 1.1: 7-segment displays component to use with MATLAB App Designer
    Upgrade of the full project to R2020a.
    Added of a rounded 7-segment mask.

--- Image/ImportMaskImage.m
+++ Image/ImportMaskImage.m
@@ -5,38 +5,59 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear variables; clc
 
-% Select the tilted version of the 7-segment (true) or the normal one (false)
-TILT = false;
-if (TILT) %#ok<*UNRCH>
-  maskImage = 'SevenSegmentDisplayTilted.png';
-else
-  maskImage = 'SevenSegmentDisplay.png';
+% Select the straight '1', tilted '2' or rounded '3' version of the 7-segment
+TYPE = 1;
+switch (TYPE)
+  case 1
+    maskImage = 'SevenSegmentDisplayStraight.png';
+  case 2
+    maskImage = 'SevenSegmentDisplayTilted.png';
+  case 3
+    maskImage = 'SevenSegmentDisplayRounded.png';
+  otherwise
+    maskImage = '';
 end
 
-% Convert the mask image into Black and White representation
-BW = logical(rgb2gray(imread(maskImage)));
-% Identify all areas from the mask
-[~,labels] = bwboundaries(BW,'noholes');
-% Assign right labels values to the area to match the 7 segment display layout
-if (~TILT)
-  labels(labels == 1) = -6;
-  labels(labels == 2) = -5;
-  labels(labels == 3) = -7;
-  labels(labels == 4) = -1;
-  labels(labels == 5) = -4;
-  labels(labels == 6) = -2;
-  labels(labels == 7) = -3;
-  labels(labels == 8) = -8;
+if ~isempty(maskImage)
+  % Convert the mask image into Black and White representation
+  BW = logical(rgb2gray(imread(maskImage)));
+  % Identify all areas from the mask
+  [~,labels] = bwboundaries(BW,'noholes');
+  % Assign right labels values to the area to match the 7 segment display layout
+  switch (TYPE)
+    case 1
+      labels(labels == 1) = -6;
+      labels(labels == 2) = -5;
+      labels(labels == 3) = -7;
+      labels(labels == 4) = -1;
+      labels(labels == 5) = -4;
+      labels(labels == 6) = -2;
+      labels(labels == 7) = -3;
+      labels(labels == 8) = -8;
+    case 2
+      labels(labels == 1) = -5;
+      labels(labels == 2) = -6;
+      labels(labels == 3) = -4;
+      labels(labels == 4) = -7;
+      labels(labels == 5) = -1;
+      labels(labels == 6) = -3;
+      labels(labels == 7) = -2;
+      labels(labels == 8) = -8;
+    case 3
+      labels(labels == 1) = -5;
+      labels(labels == 2) = -4;
+      labels(labels == 3) = -6;
+      labels(labels == 4) = -1;
+      labels(labels == 5) = -7;
+      labels(labels == 6) = -3;
+      labels(labels == 7) = -2;
+      labels(labels == 8) = -8;
+    otherwise
+      % Should not get here
+  end
+  labels = -labels;
+  % Save labelled mask
+  save('..\UIClass\SevenSegmentMask.mat','labels');
 else
-  labels(labels == 1) = -5;
-  labels(labels == 2) = -6;
-  labels(labels == 3) = -4;
-  labels(labels == 4) = -7;
-  labels(labels == 5) = -1;
-  labels(labels == 6) = -3;
-  labels(labels == 7) = -2;
-  labels(labels == 8) = -8;
+  disp('Wrong type of mask selected.');
 end
-labels = -labels;
-% Save labelled mask
-save('..\UIClass\SevenSegmentMask.mat','labels');
